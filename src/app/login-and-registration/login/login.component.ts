cc64d0d5dac1ef90dbad13ac9adb5c239d72aad2
vlad_haiduk <lolly1bomb2@gmail.com>
2024-08-30 03:22:57 +0300

    Finished ui

--- src/app/login-and-registration/login/login.component.ts
+++ src/app/login-and-registration/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {NgIconsModule} from "@ng-icons/core";
+// import { bootstrapChevronLeft } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    // NgIconsModule.withIcons({bootstrapChevronLeft})
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
