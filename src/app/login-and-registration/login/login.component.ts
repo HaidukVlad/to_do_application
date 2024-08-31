0208151877e80c491f363a5b2a942f48e5a9da37
vlad_haiduk <lolly1bomb2@gmail.com>
2024-09-01 01:30:27 +0300

    Added some animations, icons and started with form functions

--- src/app/login-and-registration/login/login.component.ts
+++ src/app/login-and-registration/login/login.component.ts
@@ -1,16 +1,45 @@
 import { Component } from '@angular/core';
-// import {NgIconsModule} from "@ng-icons/core";
-// import { bootstrapChevronLeft } from '@ng-icons/bootstrap-icons';
+import { NgIconsModule } from '@ng-icons/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    // NgIconsModule.withIcons({bootstrapChevronLeft})
+    NgIconsModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  public username: string = '';
+  public password: string = '';
+
+  public shortUsernameErrorMessage: string = 'Имя пользователя слишком короткое';
+  public longUsernameErrorMessage: string = 'Имя пользователя слишком длинное';
+
+  public shortPasswordErrorMessage: string = 'Пароль слишком короткий';
+  public longPasswordErrorMessage: string = 'Пароль слишком длинный';
+  public incorrectPasswordErrorMessage: string = 'Пароль должен содержать цифры и буквы латинского алфавита';
+
+  public onUsernameChange() {
+    if(this.username != '' && this.password != '') {
+      document.querySelector('.loginBtn')?.removeAttribute('disabled');
+    } else {
+      document.querySelector('.loginBtn')?.setAttribute('disabled', '');
+    }
+
+    if(this.username.length > 0 && this.username.length < 3) {
+    }
+  }
+
+  public onPasswordChange() {
+    if(this.username != '' && this.password != '') {
+      document.querySelector('.loginBtn')?.removeAttribute('disabled');
+    } else {
+      document.querySelector('.loginBtn')?.setAttribute('disabled', '');
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideIcons } from '@ng-icons/core';
+import { heroChevronLeft } from '@ng-icons/heroicons/outline';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const iconsProvider = provideIcons({ heroChevronLeft });
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    iconsProvider
+  ]
+})
+.catch((err) => console.error(err));
