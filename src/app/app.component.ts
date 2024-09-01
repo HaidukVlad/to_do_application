eb05ee9eb7051e7cdf2aee43906ff1213ccbbe2e
Andrew <andrej.a.vanin@gmail.com>
2024-09-01 03:14:06 +0300

    IntroPage without back to start button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroPagesComponent } from './intro-pages/intro-pages.component';
+// import { NgIconComponent, provideIcons } from '@ng-icons/core';
+// import { featherAirplay } from '@ng-icons/feather-icons';
+// import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroPagesComponent],
+  // imports: [RouterOutlet, IntroPagesComponent, NgIconComponent],
+  // viewProviders: [provideIcons({ featherAirplay, heroUsers })],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/intro-pages/intro-pages.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-intro-pages',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './intro-pages.component.html',
+  styleUrl: './intro-pages.component.css',
+})
+export class IntroPagesComponent implements OnInit {
+
+
+  showFirstDiv: boolean = true;
+  showSecondDiv: boolean = false;
+  showThridDiv: boolean = false;
+  currentSlide: number = 1;
+  currenttime: number =0;
+  button1: any = 'Next';
+  currentIntro: number = 1
+
+  
+
+  ngOnInit(): void {
+    // 
+    setTimeout(() => {
+      this.currenttime=5
+    }, 5);
+    setTimeout(() => {
+      this.showFirstDiv = false;
+      this.showSecondDiv = true;
+      this.currentIntro = 2;
+      setTimeout(()=> {
+        this.currenttime=2006
+      }, 1);
+      
+    }, 2005);
+
+  }
+
+
+  isIntro(n: number): boolean{
+    return this.currentIntro === n;
+  }
+  isgetStarted(n: number): boolean{
+    return this.currentIntro === n;
+  }
+
+  animStarted(time: number): boolean{
+    return this.currenttime===time
+  }
+
+  introShows(time: number): boolean{
+    return this.currenttime===time
+  }
+
+  nextSlide(button: HTMLButtonElement): void {
+    if(this.currentSlide == 2){
+      button.innerText = 'Get Started';
+      this.button1=button;
+    }
+    if (this.currentSlide < 3) {
+      this.currentSlide++;
+    } else if(this.currentSlide == 3){
+      this.lastIntroPage()
+    }
+  }
+
+  previousSlide(): void {
+    if (this.currentSlide > 1) {
+      this.currentSlide--;
+    }
+    if (this.button1.innerText != 'Next') {
+      this.button1.innerText = 'Next';
+    }
+  }
+
+  isActiveSlide(slideNumber: number): boolean {
+    return this.currentSlide === slideNumber;
+  }
+
+  backIntro(): void{ 
+    this.currentIntro--;
+  }
+  lastIntroPage(): void{
+    // this.showSecondDiv = false;
+    // this.showThridDiv = true;
+    
+    this.currentIntro = 3;
+    setTimeout(()=> {
+      this.currenttime=13
+    }, 1);
+    
+  }
+
+}
