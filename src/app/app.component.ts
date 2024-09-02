c07e64be73debf542858c3bd702f01c3a35f63e0
vlad_haiduk <lolly1bomb2@gmail.com>
2024-09-02 11:19:18 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginAndRegistrationModule } from './login-and-registration/login-and-registration.module';
 import { LoginComponent } from "./login-and-registration/login/login.component";
+import { RegistrationComponent } from "./login-and-registration/registration/registration.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginAndRegistrationModule,LoginComponent],
+  imports: [RouterOutlet, LoginAndRegistrationModule, LoginComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
