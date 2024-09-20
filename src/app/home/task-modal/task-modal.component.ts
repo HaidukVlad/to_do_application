4d908018dee71af6ecc3cfdfcfdb25ccb3c7a474
vlad_haiduk <lolly1bomb2@gmail.com>
2024-09-20 14:07:30 +0300

    adde buttons to modal winndow

--- src/app/home/task-modal/task-modal.component.ts
+++ src/app/home/task-modal/task-modal.component.ts
@@ -1,9 +1,14 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { cssTime, cssTag, cssFlagAlt, cssArrowRightO } from '@ng-icons/css.gg';
 
 @Component({
   selector: 'app-task-modal',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  providers: [
+    provideIcons({ cssTime, cssTag, cssFlagAlt, cssArrowRightO })
+  ],
   templateUrl: './task-modal.component.html',
   styleUrl: './task-modal.component.css'
 })
